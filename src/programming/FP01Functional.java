fe66e336a79eb90e6172e33b1d719698474333b5
Renan Santos Gomes <github@renansgomes.com.br>
2023-05-30 15:25:04 -0300

    Step 06 - Using map in functional programs

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -10,7 +10,10 @@ public class FP01Functional {
 		//printOddNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
 		//printAllCoursesIndividually(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
 		//printCoursesContainingSpringWord(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
-		printCoursesContainingAtLeastFourLetters(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
+		//printCoursesContainingAtLeastFourLetters(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
+		//printSquaresOfEvenNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		//printCubesOfOddNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		printNumbersOfCaractersEachCourseName(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
 	}
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
@@ -43,6 +46,26 @@ public class FP01Functional {
 	private static void printCoursesContainingAtLeastFourLetters(List<String> courses) {
 		courses.stream()
 			.filter(course -> course.length() >= 4)
+			.forEach(System.out::println);
+	}
+	
+	private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream()
+			.filter(number -> number % 2 == 0)
+			.map(number -> number * number)
+			.forEach(System.out::println);
+	}
+	
+	private static void printCubesOfOddNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream()
+			.filter(number -> number % 2 != 0)
+			.map(number -> number * number * number)
+			.forEach(System.out::println);
+	}
+	
+	private static void printNumbersOfCaractersEachCourseName(List<String> courses) {
+		courses.stream()
+			.map(course -> course.length())
 			.forEach(System.out::println);
 	}
 }
