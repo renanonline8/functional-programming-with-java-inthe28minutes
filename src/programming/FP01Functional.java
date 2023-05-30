c26015deb2f66a4793f720ca99ecd783498442f0
Renan Santos Gomes <github@renansgomes.com.br>
2023-05-30 12:09:54 -0300

    Step 05 - Do Functional Programming Exercises with Streams, Filters and Lambdas

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -6,7 +6,11 @@ public class FP01Functional {
 	
 	public static void main(String[] args) {
 		//printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
-		printEvenNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		//printEvenNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		//printOddNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		//printAllCoursesIndividually(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
+		//printCoursesContainingSpringWord(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
+		printCoursesContainingAtLeastFourLetters(List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"));
 	}
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
@@ -16,6 +20,29 @@ public class FP01Functional {
 	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
 		numbers.stream()
 			.filter(number -> number % 2 == 0)
+			.forEach(System.out::println);
+	}
+	
+	private static void printOddNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream()
+			.filter(number -> number % 2 == 1)
+			.forEach(System.out::println);
+	}
+	
+	private static void printAllCoursesIndividually(List<String> courses) {
+		courses.stream()
+			.forEach(System.out::println);
+	}
+	
+	private static void printCoursesContainingSpringWord(List<String> courses) {
+		courses.stream()
+			.filter(course -> course.contains("Spring"))
+			.forEach(System.out::println);
+	}
+	
+	private static void printCoursesContainingAtLeastFourLetters(List<String> courses) {
+		courses.stream()
+			.filter(course -> course.length() >= 4)
 			.forEach(System.out::println);
 	}
 }
